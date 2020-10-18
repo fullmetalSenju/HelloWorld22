8a7ed02a522a666d2f3d4a595d0b270fbf863fc2
Nata <natalinaschneider@gmail.com>
2020-10-18 22:31:26 +0300

    hello finally

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main (String []args){
+        System.out.println("Hello world, my name is Nata");
+        System.out.println("I want to be a QA automation");
+    }
+
+}
---
+++ src/Initials.java
@@ -0,0 +1,14 @@
+public class Initials {
+    public static void main (String[]args){
+
+        System.out.println("NN     NN     MM     MM      SSSSSS ");
+        System.out.println("NNN    NN     MMМ   MMM     SS     S");
+        System.out.println("NNN    NN     MMMM MMMM       SS    ");
+        System.out.println("NN  NN NN     MM  M  MM         SS  ");
+        System.out.println("NN    NNN     MM     MM     S     SS");
+        System.out.println("NN     NN     MM     MM       SSSSS  ");
+
+
+
+    }
+}
